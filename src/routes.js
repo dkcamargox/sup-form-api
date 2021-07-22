24672871f99591aa9579fa6a3359f2a81edd35b9
Douglas Camargo <52894581+dkcamargox@users.noreply.github.com>
2021-07-22 13:45:01 -0300

    Creating survey route

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ const express = require("express");
 const routes = express.Router();
 const { getUsers, logIn } = require("./controllers/usersController");
 const { getSellers, getRoutes } = require("./controllers/sellersController");
+const { postSurvey } = require("./controllers/infosController");
 
 routes.get("/users", getUsers);
 routes.get("/sellers/:sucursal/:supervisor", getSellers);
 routes.get("/routes/:sucursal/:seller_id", getRoutes);
+routes.post("/survey", postSurvey);
 routes.post("/login", logIn);
 
 module.exports = routes;
