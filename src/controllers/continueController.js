afa9a39f1a1c8fc5b682922eb6a7bb4073dea99d
dkcamargox <camargo.douglas@icloud.com>
2021-11-06 13:16:32 -0300

    finishing to link the coaching continue

--- src/controllers/continueController.js
+++ src/controllers/continueController.js
@@ -18,13 +18,35 @@ module.exports = {
 
             console.log(request.body);
 
-            const rowId = await continueSheet.addRow({
-                'id supervisor': request.body.supervisor,
-                'id ruta': request.body.route,
-                'tipo de formulario': request.body.formType,
-                'ultimo cargado': 0
-            });
+            let rowId;
 
+            if(request.body.formType === 'coaching') {
+                rowId = await continueSheet.addRow({
+                    'id supervisor': request.body.supervisor,
+                    'id ruta': request.body.route,
+                    'tipo de formulario': request.body.formType,
+                    'ultimo cargado': 0,
+                    '¿Indaga sobre el último pedido?': 0,
+                    '¿Planifica el pedido antes de ingresar al PDV?': 0,
+                    '¿POP?': 0,
+                    '¿Verifica el stock en todas las áreas del PDV?': 0,
+                    '¿Trabaja en una mayor exposición de los productos?': 0,
+                    '¿Indaga y verifica la situación y las acciones de la competencia?': 0,
+                    '¿Comunica las acciones comerciales vigentes?': 0,
+                    '¿Realiza la propuesta de ventas, ofreciendo todos los productos?': 0,
+                    '¿Toma todos los recaudos necesarios para facilitar la entrega? (pedido, dinero, horario, etc.)': 0,
+                    '¿Renueva, coloca y pone precios al POP? Siguiendo criterios del PDV': 0,
+                    '¿Administra el tiempo de permanencia en el PDV?': 0,
+                    '¿Uso de Catálogo?': 0,
+                });
+            } else {
+                rowId = await continueSheet.addRow({
+                    'id supervisor': request.body.supervisor,
+                    'id ruta': request.body.route,
+                    'tipo de formulario': request.body.formType,
+                    'ultimo cargado': 0,
+                });
+            }
             console.log({ error: false, id: rowId['_rowNumber'] });
 
             return response
@@ -88,6 +110,21 @@ module.exports = {
             });
 
             row['ultimo cargado'] = request.body.countage;
+
+            if (row['tipo de formulario'] === 'coaching') {
+                row['¿Indaga sobre el último pedido?'] = request.body.stats.lastOrder
+                row['¿Planifica el pedido antes de ingresar al PDV?'] = request.body.stats.sellPlan
+                row['¿POP?'] = request.body.stats.pop
+                row['¿Verifica el stock en todas las áreas del PDV?'] = request.body.stats.stock
+                row['¿Trabaja en una mayor exposición de los productos?'] = request.body.stats.exposition
+                row['¿Indaga y verifica la situación y las acciones de la competencia?'] = request.body.stats.competitorSales
+                row['¿Comunica las acciones comerciales vigentes?'] = request.body.stats.sales
+                row['¿Realiza la propuesta de ventas, ofreciendo todos los productos?'] = request.body.stats.sellPropouse
+                row['¿Toma todos los recaudos necesarios para facilitar la entrega? (pedido, dinero, horario, etc.)'] = request.body.stats.deliveryPrecautions
+                row['¿Renueva, coloca y pone precios al POP? Siguiendo criterios del PDV'] = request.body.stats.popPricing
+                row['¿Administra el tiempo de permanencia en el PDV?'] = request.body.stats.timeManagement
+                row['¿Uso de Catálogo?'] = request.body.stats.catalogue
+            }
 
             await row.save();
 
@@ -167,26 +204,64 @@ module.exports = {
 
             
             const rows = await continueRows.map(row => {
-                return {
-                    id: row['_rowNumber'],
-                    route: getRouteNameById(routesRows, request.params.sucursal, row['id ruta']),
-                    seller: getSellerByRouteId(
-                        routesRows,
-                        sellersRows,    
-                        request.params.sucursal,
-                        row['id ruta']
-                    ).seller,
-                    routeId: row['id ruta'],
-                    sellerId: getSellerByRouteId(
-                        routesRows,
-                        sellersRows,    
-                        request.params.sucursal,
-                        row['id ruta']
-                    ).sellerId,
-                    formType: row['tipo de formulario'],
-                    clientCountage: row['ultimo cargado'],
-                    supervisorId: row['id supervisor']
-                };
+                if (row['tipo de formulario'] === 'relevamiento') {
+                    return {
+                        id: row['_rowNumber'],
+                        route: getRouteNameById(routesRows, request.params.sucursal, row['id ruta']),
+                        seller: getSellerByRouteId(
+                            routesRows,
+                            sellersRows,    
+                            request.params.sucursal,
+                            row['id ruta']
+                        ).seller,
+                        routeId: row['id ruta'],
+                        sellerId: getSellerByRouteId(
+                            routesRows,
+                            sellersRows,    
+                            request.params.sucursal,
+                            row['id ruta']
+                        ).sellerId,
+                        formType: row['tipo de formulario'],
+                        clientCountage: row['ultimo cargado'],
+                        supervisorId: row['id supervisor']
+                    };
+                } else {
+                    return {
+                        id: row['_rowNumber'],
+                        route: getRouteNameById(routesRows, request.params.sucursal, row['id ruta']),
+                        seller: getSellerByRouteId(
+                            routesRows,
+                            sellersRows,    
+                            request.params.sucursal,
+                            row['id ruta']
+                        ).seller,
+                        routeId: row['id ruta'],
+                        sellerId: getSellerByRouteId(
+                            routesRows,
+                            sellersRows,    
+                            request.params.sucursal,
+                            row['id ruta']
+                        ).sellerId,
+                        formType: row['tipo de formulario'],
+                        clientCountage: row['ultimo cargado'],
+                        supervisorId: row['id supervisor'],
+                        stats: {
+                            lastOrder: Number(row['¿Indaga sobre el último pedido?']),
+                            sellPlan: Number(row['¿Planifica el pedido antes de ingresar al PDV?']),
+                            pop: Number(row['¿POP?']),
+                            stock: Number(row['¿Verifica el stock en todas las áreas del PDV?']),
+                            exposition: Number(row['¿Trabaja en una mayor exposición de los productos?']),
+                            competitorSales: Number(row['¿Indaga y verifica la situación y las acciones de la competencia?']),
+                            sales: Number(row['¿Comunica las acciones comerciales vigentes?']),
+                            sellPropouse: Number(row['¿Realiza la propuesta de ventas, ofreciendo todos los productos?']),
+                            deliveryPrecautions: Number(row['¿Toma todos los recaudos necesarios para facilitar la entrega? (pedido, dinero, horario, etc.)']),
+                            popPricing: Number(row['¿Renueva, coloca y pone precios al POP? Siguiendo criterios del PDV']),
+                            timeManagement: Number(row['¿Administra el tiempo de permanencia en el PDV?']),
+                            catalogue: Number(row['¿Uso de Catálogo?'])
+                        }
+                    };
+                    
+                }
             }).filter(row => {
                 return `${row.supervisorId}` === `${request.params.supervisor}`
             });
